7d9d78c31c43389c9d3be479dfb15f696fb40509
Aaban Saad <aabansaad@gmail.com>
2023-10-10 20:44:39 +0600

    added title and icon

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 	
+import java.nio.channels.NonWritableChannelException;
+
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 
 
 
@@ -17,6 +21,9 @@ public class Main extends Application {
 			Scene scene = new Scene(root,1000,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Gradebook - alpha");
+			Image iconImage = new Image("icon.png");
+			primaryStage.getIcons().add(iconImage);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
